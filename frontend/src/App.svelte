e23e9ea540b1cc62a50bc7f354500941bafc1cfd
Ryan Gibson <gibsorya@gmail.com>
2023-05-10 17:10:32 -0500

    create basic navbar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,7 +9,7 @@
   import { auth0 } from "./lib/services/auth0";
   import { onMount } from "svelte";
   import Callback from "./pages/Callback.svelte";
-    import PrivateRoute from "./components/auth/PrivateRoute.svelte";
+  import PrivateRoute from "./components/auth/PrivateRoute.svelte";
 
   let { isLoading, isAuthenticated, login, initializeAuth0, logout } = auth0;
 
@@ -23,13 +23,13 @@
     );
   };
 
-  const handleLogin = async() => {
+  const handleLogin = async () => {
     await login();
-  }
+  };
 
-  const handleLogout = async() => {
+  const handleLogout = async () => {
     await logout();
-  }
+  };
 
   onMount(async () => {
     await initializeAuth0({
@@ -37,42 +37,33 @@
       clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
       redirectUri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
       audience: import.meta.env.VITE_AUTH0_AUDIENCE || undefined,
+      onRedirectCallback,
     });
   });
 </script>
 
-<main class="container mx-auto">
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-  {#if $isAuthenticated}
-    <button id="logout" on:click|preventDefault={handleLogout}>Click to Logout</button>
-  {:else}
-    <button id="login" on:click|preventDefault={handleLogin}>Click to Login</button>
-  {/if}
-
-  <!-- <div class="card">
-    <Counter />
-  </div> -->
-
+<main>
   <Router>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/users">Users</Link>
+    <nav class="nav flex justify-between items-center">
+      <div class="flex">
+        <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
+      </div>
+      <div class="mr-4">
+        {#if $isAuthenticated}
+          <button id="logout" on:click|preventDefault={handleLogout}
+            >Logout</button
+          >
+        {:else}
+          <button id="login" on:click|preventDefault={handleLogin}
+            >Login</button
+          >
+        {/if}
+      </div>
     </nav>
     <div>
       <Route path="/">
         <PlayerTable />
-      </Route>
-      <Route path="/about">
-        <About />
       </Route>
       <Route path="/users">
         <h1>Users</h1>
@@ -85,32 +76,4 @@
       <Route path="/callback" component={Callback} />
     </div>
   </Router>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
